40495efe6fd978e8002bbf3550f1e46c9541d49c
TAHSIN NOOR SHOVON <122145199+Shovon211-015@users.noreply.github.com>
2023-01-08 22:27:57 +0600

    Delete Hospitalmanagementsystem directory

--- Hospitalmanagementsystem/HospitalManagementSystem.java
+++
@@ -1,9 +0,0 @@
-
-package Hospitalmanagementsystem;
-
-public class HospitalManagementSystem {
-    public static void main(String[] args) {
-        MenuPage a = new MenuPage();
-        a.setVisible(true);
-    }
-}
